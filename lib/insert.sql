a7fe29fe6c84d79b4640acc88ddb49d6262fad5d
Grace Nieboer <grace.nieboer@outlook.com>
2023-03-18 10:37:01 -0400

    14/14 tests complete

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Mr. Chocolate', 3, 'M', 'Brown', 'Silly', 1),
+    ('Rowdy', 3, 'M', 'Brown', 'Silly', 1),
+    ('Tabitha', 3, 'F', 'Brown', 'Silly', 1),
+    ('Sergeant Brown', 3, 'M', 'Brown', 'Silly', 1),
+    ('Melissa', 3, 'F', 'Brown', 'Silly', 1),
+    ('Grinch', 3, 'M', 'Brown', 'Silly', 1),
+    ('Wendy', 3, 'F', 'Brown', 'Silly', 1),
+    (null, 3, 'M', 'Brown', 'Silly', 1);
